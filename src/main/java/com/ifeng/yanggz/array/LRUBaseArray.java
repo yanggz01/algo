1051e1fd083ab66e88ec6e2f00464b561e3e8771
yanggz <yanggz@ifeng.com>
2019-12-27 15:05:12 +0800

    add LRUBaseLinkedList

---
+++ src/main/java/com/ifeng/yanggz/array/LRUBaseArray.java
@@ -0,0 +1,121 @@
+package com.ifeng.yanggz.array;
+
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 基于数组实现的LRU缓存
+ *
+ * @Author yanggz
+ * @Date 2019-12-23
+ * 1、空间复杂度O(n)
+ * 2、时间复杂度O(n)
+ * 3、不支持null的缓存
+ */
+public class LRUBaseArray<T> {
+    private static final int DEFAULT_CAPACITY = (1 << 3);
+
+    private T[] value;
+
+    private int count;
+
+    private int capacity;
+
+    private Map<T, Integer> holder;
+
+    public LRUBaseArray() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public LRUBaseArray(int capacity) {
+        this.capacity = capacity;
+        value = (T[]) new Object[capacity];
+        count = 0;
+        holder = new HashMap(capacity);
+    }
+
+    /**
+     * 插入缓存
+     *
+     * @param value
+     */
+    public void offer(T value) {
+        if(value == null) {
+            throw new InvalidParameterException("Value can not be null!");
+        }
+        Integer index = holder.get(value);
+        if(index == null) {
+            if(isFull()) {
+                // 删除最老的，然后缓存
+                removeAndCache(value);
+            } else {
+                // 缓存
+                cache(value, count);
+            }
+        } else {
+            // 更新缓存索引
+            update(index);
+        }
+    }
+
+    public void cache(T object, int end) {
+        // 右移
+        rightShift(end);
+        count++;
+        value[0] = object;
+        holder.put(object, 0);
+    }
+
+    /**
+     * end 前的数据都向右移动一位
+     * @param end
+     */
+    public void rightShift(int end) {
+        for(int i=end-1; i>=0; i++) {
+            value[i+1] = value[i];
+            holder.put(value[i], i+1);
+        }
+    }
+
+    public void removeAndCache(T object) {
+        T key = value[count--];
+        holder.remove(key);
+        cache(object, count);
+    }
+
+    public boolean isFull() {
+        return count == capacity;
+    }
+
+    public boolean empty() {
+        return count == 0;
+    }
+
+
+    /**
+     * 更新索引
+     *
+     * @param index
+     */
+    private void update(Integer index) {
+        T key = value[index];
+        rightShift(index);
+        value[0] = key;
+        holder.put(key, 0);
+    }
+
+    public boolean isContains(T object) {
+        return holder.containsKey(object);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i=0; i<count; i++) {
+            stringBuilder.append(value[i]);
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/com/ifeng/yanggz/linkedlist/LRUBaseLinkedList.java
@@ -0,0 +1,133 @@
+package com.ifeng.yanggz.linkedlist;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+/**
+ * 基于链表的缓存淘汰算法（LRU）
+ * @author yanggz
+ * @date 2019-12-23
+ *
+ */
+public class LRUBaseLinkedList<T> {
+
+    private static final Integer DEFAULT_CAPACITY = 10;
+
+    private SNode<T> headNode;
+
+    private int length;
+
+    private int capacity;
+
+    public LRUBaseLinkedList() {
+        this.capacity = DEFAULT_CAPACITY;
+        this.length = 0;
+        this.headNode = new SNode<T>();
+    }
+
+    public LRUBaseLinkedList(int capacity) {
+        this.capacity = capacity;
+        this.length = 0;
+        this.headNode = new SNode<T>();
+    }
+
+    public void add(T data) {
+        SNode preNode = findPreNode(data);
+        if(preNode != null) {
+            // 删除该节点
+            deleteNextElem(preNode);
+            // 插入该节点到头部
+            insertIntoHead(data);
+        } else {
+            if(length == capacity) {
+                // 删除尾节点
+                deleteTailElem();
+            }
+            // 插入头节点
+            insertIntoHead(data);
+        }
+    }
+
+    private void deleteTailElem() {
+        SNode sNode = headNode;
+        while (sNode.getNext().getNext() != null) {
+            sNode = sNode.getNext();
+        }
+        SNode tmp = sNode.getNext();
+        sNode.setNext(null);
+        tmp = null;
+        length--;
+    }
+
+    private void insertIntoHead(T data) {
+        SNode sNode = headNode.getNext();
+        headNode.setNext(new SNode(data, sNode));
+        length++;
+    }
+
+    private void deleteNextElem(SNode preNode) {
+        SNode sNode = preNode.getNext();
+        preNode.setNext(sNode.getNext());
+        sNode = null;
+        length--;
+    }
+
+    private SNode findPreNode(T data) {
+
+        SNode sNode = headNode;
+        while (sNode.getNext() != null) {
+            if(data.equals(sNode.getNext().getElement())) {
+                return sNode;
+            }
+            sNode = sNode.next;
+        }
+        return null;
+    }
+
+    public void printAll() {
+        SNode sNode = headNode.getNext();
+        while (sNode != null) {
+            System.out.print(sNode.getElement());
+            if(sNode.getNext() != null) {
+                System.out.print(",");
+            }
+            sNode = sNode.getNext();
+        }
+        System.out.println();
+    }
+
+    public class SNode<T>{
+
+        private T element;
+
+        private SNode next;
+
+        public SNode(T element) {
+            this.element = element;
+        }
+
+        public SNode() {
+            this.next = null;
+        }
+
+        public SNode(T element, SNode next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public void setElement(T element) {
+            this.element = element;
+        }
+
+        public SNode getNext() {
+            return next;
+        }
+
+        public void setNext(SNode next) {
+            this.next = next;
+        }
+    }
+}
